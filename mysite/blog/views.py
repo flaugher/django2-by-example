93940180c3b52ebc375b5ef44ca575c01dbf4996
Robert Flaugher <rlfprog@gmail.com>
2019-11-28 18:24:28 -0800

    Run isort on blog/views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,6 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import (
-    render, get_object_or_404,
-)
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
