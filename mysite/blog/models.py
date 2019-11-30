5eb8fad958492930c322200d9fdda002409cd25f
Robert Flaugher <rlfprog@gmail.com>
2019-11-29 16:39:10 -0800

    Add Comment model to admin site

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,7 +17,7 @@ class Comment(models.Model):
     email = models.EmailField()
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     # Use for deactivating inappropriate comments.
     active = models.BooleanField(default=True)
 
