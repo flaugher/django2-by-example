3757df5406034a5bddf1d3ef988d509d37edbd3f
Robert Flaugher <rlfprog@gmail.com>
2019-12-14 14:40:55 -0800

    Fix logout problem by changing name of logout template

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
+    # account/login/
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    # account/logout/
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # account/dashboard/
     path('', views.dashboard, name='dashboard'),
