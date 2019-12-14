e8679a6c58ce800f5ba565fedaa7ed1d50085bdc
Robert Flaugher <rlfprog@gmail.com>
2019-12-14 08:50:45 -0800

    Add change password and change password done features

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     # account/dashboard/
-    # Uses setting LOGIN_REDIRECT_URL
     path('', views.dashboard, name='dashboard'),
+    # account/password_change/
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # account/password_change/done/
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
