3b37aafe3788a6d2f11f227d1c3f1e4df938fc9a
Robert Flaugher <rlfprog@gmail.com>
2019-12-17 11:58:52 -0800

    Create images app and build Image model

---
+++ bookmarks/images/models.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.db import models
+from django.utils.text import slugify
+
+
+class Image(models.Model):
+    """Store images bookmarked from other websites."""
+    # A user can post multiple images but each image is posted by only one user.
+    # If the user is deleted, delete all their images.
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+        related_name='images_created',
+        on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    # SEO-friendly URL
+    slug = models.SlugField(max_length=200, blank=True)
+    # Original URL for the image
+    url = models.URLField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    # Create a database index for the created field.
+    created = models.DateField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        """Automatically generate image slug from the title.
+
+        Only do this when a slug isn' provided.
+        """
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Image, self).save(*args, **kwargs)
