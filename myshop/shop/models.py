4c384170fc6ac56ade3121278bc607ed28d057ea
Robert Flaugher <rlfprog@gmail.com>
2020-01-01 15:15:04 -0800

    Define Category and Product models

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    # Unique implies creation of an index.
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+        related_name='products',
+        on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    # Unique implies creation of an index.
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    # Always use DecimalField, not FloatField, for money to avoid rounding issues.
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        # Create an index that combines the id and slug fields.
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
