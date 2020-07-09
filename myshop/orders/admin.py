23d298641a9c947b7e8900525b135050ec614ee4
Robert Flaugher <rlfprog@gmail.com>
2020-07-09 16:48:39 -0700

    Add custom admin action to Admin site

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,9 +1,59 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+
 from .models import Order, OrderItem
+
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+# howto: create custom django admin action
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/actions/
+# Also: https://medium.com/@hakibenita/how-to-add-custom-action-buttons-to-django-admin-8d266f5b0d41
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    # Set up response to tell browser to treat is as CSV file.
+    response = HttpResponse(content_type='text/csv')
+
+    # Indicate that response contains an attachment.
+    response['Content-Disposition'] = 'attachment;' \
+        'filename={}.csv'.format(opts.verbose_name)
+
+    # Create writer object that writes to the response.
+    writer = csv.writer(response)
+
+    # Get the model's fields dynamically using get_fields() from its _meta options.
+    fields = [field for field in opts.get_fields(
+    ) if not field.many_to_many and not field.one_to_many]
+
+    # Write a first row with header information.
+    writer.writerow([field.verbose_name for field in fields])
+
+    # Write data rows.
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+
+            # Be sure to convert any date objects to strings.
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+
+            data_row.append(value)
+        writer.writerow(data_row)
+
+    return response
+
+
+# Customize the display name.
+export_to_csv.short_description = 'Export to CSV'
+
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -12,3 +62,5 @@ class OrderAdmin(admin.ModelAdmin):
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    # Add generic admin action created above to the OrderAdmin class.
+    actions = [export_to_csv]
